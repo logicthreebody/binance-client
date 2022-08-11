8fd8e6ae4abc9253182cb9e6386a13f06db172d3
logic <test@gmail.com>
2022-08-11 16:27:22 +0800

    update .gitignore

--- src/test/java/com/binance/client/examples/constants/PrivateConfig.java
+++
@@ -1,8 +0,0 @@
-package com.binance.client.examples.constants;
-
-public class PrivateConfig {
-
-    public static final String API_KEY = "c14efab22d9ac95f6695c3aff6a6ac7153ea24d8734587ea83482e0b1e2a57a2";
-    public static final String SECRET_KEY = "44f5454ad165d5f9be1671e764e9da65519cde6e881d2c5b98cdee21fcdcff89";
-
-}
